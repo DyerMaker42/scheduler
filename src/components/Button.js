8722f72bc6a4738653c4ec286ad7557f6dd2abd3
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 15:46:09 -0700

    reformatted button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,22 +4,25 @@ import "components/Button.scss";
 
 export default function Button(props) {
    let buttonClass = "button";
-   let disableClass = false;
+  
    if (props.confirm){
       buttonClass += " button--confirm";
    } 
    if (props.danger){
       buttonClass += " button--danger";
    }
-   if (props.onClick){
-      ;
-   }
-   if (props.disabled){
-       disableClass = true;
-   }
+  
    
    
-   return <>
-          <button disabled={disableClass} className={buttonClass}>{props.children}</button>
-         </>;
+   return (
+   <>
+          <button 
+          onClick={props.onClick}
+          disabled={props.disabled} 
+          className={buttonClass}
+          >
+             {props.children}
+             </button>
+         </>
+   );
 }
