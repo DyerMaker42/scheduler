44ee58366ee6bbe5c82735d6595e41172c5aae77
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 14:54:32 -0700

    undid changes I should not have made

--- stories/index.js
+++ stories/index.js
@@ -1,32 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
-import "./styles.css";
+
 import Button from "components/Button";
-import tweets from "./tweets.json"
-
-const tweet = tweets[0];
-
-function Tweet(props) {
-  <article className="tweet">
-  <header className="tweet__header">
-    <img
-      className="tweet__header-avatar"
-      src=""
-      alt="User Avatar"
-    />
-    <h2 className="tweet__header-name"></h2>
-  </header>
-  <main className="tweet__content">
-    <p></p>
-  </main>
-  <footer className="tweet__footer"></footer>
-</article>
-}
 
 storiesOf("Button", module)
   .addParameters({
