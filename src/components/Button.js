fa9b3ee88061a07594fa7d0ca30f75682adbac0f
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 16:52:20 -0700

    refacoted button with classNames

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,30 +2,21 @@ import React from "react";
 
 import "components/Button.scss";
 
-const classNames = require('classnames');
+let classNames = require('classnames');
 
 export default function Button(props) {
-   let buttonClass = "button";
-  
-   if (props.confirm){
-      buttonClass += " button--confirm";
-   } 
-   if (props.danger){
-      buttonClass += " button--danger";
-   }
-   
-   classNames('') 
-   
-   
+   const buttonClass = classNames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
    return (
-   <>
-          <button 
-          onClick={props.onClick}
-          disabled={props.disabled} 
-          className={buttonClass}
-          >
-             {props.children}
-             </button>
-         </>
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
    );
-}
+ }
