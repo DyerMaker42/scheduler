f4e20df5e7afd33ab1bd4f27f12107a22efa2b70
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 09:03:51 -0700

    imported tweets from tweets.json

--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,7 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import Tweets from "tweets.json"
 
 storiesOf("Button", module)
   .addParameters({
