933ee58469e7a9117165b3bdf61780b8aeaf018d
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 18:09:36 -0700

    imported class names into daylist item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import "components/DayListItem.scss";
 
+let classNames = require('classnames');
+
 export default function DayListItem(props) {
-  
+  const selectItem = class
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+    className= {selectItem} 
+    onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
