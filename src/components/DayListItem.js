72db47581e86050ff99a28b5f6690f238e10175e
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 18:27:01 -0700

    fixed day list selector logic

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,11 +5,14 @@ import "components/DayListItem.scss";
 let classNames = require('classnames');
 
 export default function DayListItem(props) {
-  const selectItem = class
+  const selectClass = classNames("day-list__item",
+  {"day-list__item--selected":props.selected},
+  {"day-list__item--full": props.spot == 0}
+)
 
   return (
     <li 
-    className= {selectItem} 
+    className= {selectClass} 
     onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
