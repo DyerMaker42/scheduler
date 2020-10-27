e9e7164056a47f3e1afbea9bd67843718a04de4d
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 20:46:35 -0700

    adjusts add day item

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,5 +1,29 @@
 import React from "react";
-
+import DayListItem from "./DayListItem";
 export default function DayList(props){
-  return <ul></ul>;
+  
+  // props.map(day => {
+  //   <DayListItem
+  //     name={day.name}
+  //    spots={day.spots}
+  //    selected={day.name === props.day}
+  //    setDay={day.setDay}
+  //    key={day.id}
+  //   />
+  // })
+  console.log(props, 'props')
+  
+
+  return (
+  <ul>
+    {props.days.map(day => {
+    return (
+      <DayListItem 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}  />)
+  })}
+  </ul>
+  );
 }
