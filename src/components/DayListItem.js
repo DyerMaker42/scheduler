eafbf22f778196d3ca804630cb95c2abc0db9a2a
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-26 19:53:29 -0700

    debugged daylistitem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,13 +9,26 @@ export default function DayListItem(props) {
   {"day-list__item--selected":props.selected},
   {"day-list__item--full": props.spot == 0}
 )
+  /*refactor later put inside a functin*/ 
+  
+    let number;
+    if(props.spots > 1){
+    number = props.spots + ' spots remaining';
+    } else if( props.spots === 0){
+    number = 'no spots remaining';
+    } else if(props.spots === 1) {
+    number = '1 spot remaining';
+    }
+   
+
+
 
   return (
     <li 
     className= {selectClass} 
     onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{number}</h3>
     </li>
   );
 }
