858648bfc2e9e4124b734b7f56da20d1f617de9d
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-10-27 19:47:26 -0700

    solified interviewer list item and made skeleton for interviewerlist

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,21 +1,46 @@
 import React from "react";
 
+import "components/InterviewerListItem.scss"
+
 let classNames = require('classnames');
 
-export default function InterviewerListItem(props) {
-  
 
+export default function InterviewerListItem(props) {
+  //   id:number - the id of the interviewer
+  // name:string - the name of the interviewer
+  // avatar:url - a url to an image of the interviewer
+  // selected:boolean - to determine if an interview is selected or not
+  // setInterviewer:function - sets the interviewer upon selection
+
+
+  // console.log(props, "props");
+
+  const classItem = classNames("interviewers__item",
+    { "interviewers__item--selected":props.selected},
+    {}
+
+  );
+  //event handler
+ const onClick = ((click)=>{
+  props.setInterviewer()
+  console.log('cats')
+ })
+  //set class
 
   return (
-    <ul>
-      <li className="interviewers__item">
-        <img
-          className="interviewers__item-image"
-          src="https://i.imgur.com/LpaY82x.png"
-          alt="Sylvia Palmer"
-        />
-      Sylvia Palmer
-      </li>
-    </ul>
+
+
+
+    <li className={classItem} onClick={onClick}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+
+
+
   )
 }
