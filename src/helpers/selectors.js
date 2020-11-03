52c38ede1752d3db561f881e732d9062486c14ad
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-11-02 19:46:01 -0800

    refactored selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,7 +6,7 @@ export function getAppointmentsForDay(state, day) {
   // }
   
   const filteredDays = state.days.filter(appointment => appointment.name === day);
-  
+  //get a code review added test to add a .id, don't think I was supposed to do that
    console.log("filtered days", filteredDays)
   //  if (!filteredDays[0].appointments){
   //    return filteredDays[0]
