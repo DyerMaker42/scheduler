d0acb6386405ad585d0869a65d4bd58ff0eec82d
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-11-02 18:07:14 -0800

    refactored filterd days functin

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,5 +2,5 @@ import react from 'react';
 
 export default function getAppointmentsByDay(state, day){
   const filteredDays = state.filter(appointment => appointment.name === day );
-  return filteredDays
+  return filteredDays;
 }
