2e5acb51ac31b6b872d206332909f3f504d4cf62
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-11-03 19:02:27 -0800

    added constants empty and show to apppointment index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,16 @@ import "components/Appointment/styles.scss"
 
 import "components/Button"
 
-import Header from "./Header";
+import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode"
 
 export default function Appointment(props){
   const interviewTrue = props.interview;
   const ShowOrGo = interviewTrue ? <Show name={props.interview.student} interviewer={props.interview.interviewer}/>:<Empty/>;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
   
   return (
     <>
