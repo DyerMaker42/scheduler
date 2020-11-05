af9c0fc41fb05ad614dd26ec689c9bca251481bb
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-11-04 22:02:56 -0800

    refactored interviewer, interviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,15 +6,6 @@ let classNames = require('classnames');
 
 
 export default function InterviewerListItem(props) {
-  //   id:number - the id of the interviewer
-  // name:string - the name of the interviewer
-  // avatar:url - a url to an image of the interviewer
-  // selected:boolean - to determine if an interview is selected or not
-  // setInterviewer:function - sets the interviewer upon selection
-
-
-  // console.log(props, "props");
-
   const interviewerClass = classNames("interviewers__item",
     { "interviewers__item--selected": props.selected },
   );
