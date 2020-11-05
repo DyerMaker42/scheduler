0324d32febe53ddd8f98bdcfc45b6164acae8138
Nicholas Kotsos <nicholaskotsos@Nicholass-MacBook-Air.local>
2020-11-04 22:06:15 -0800

    updated useVisMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,13 +1,13 @@
-import {useState } from "react";
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  
+
   function transition(newMode, replace) {
     if (replace) {
       history.pop();
-      
+
       setHistory([...history, newMode]);
     } else {
       setHistory([...history, newMode])
@@ -15,10 +15,8 @@ export default function useVisualMode(initial) {
     setMode(newMode);
   }
   function back() {
-    //returns true if hist length >1
-    // let histBoo = history.length > 1;
-    // let histSub = history.length - 1;
-    if ( history.length > 1) {
+
+    if (history.length > 1) {
       history.pop();
       setMode(history[history.length - 1]);
     }
