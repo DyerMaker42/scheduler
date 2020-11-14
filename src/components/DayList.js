73116b6d07e16745fb614d3767399ce458510621
Nicholas Kotsos <nicholaskotsos@nicholass-air.lan>
2020-11-14 15:58:38 -0800

    refactored day list and day list item

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,26 +1,32 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 export default function DayList(props) {
-
-
   return (
     <ul>
-      {props.days.map(day => {
-        const spots = Object.values(props.appointments).reduce((accumulator, appointment) => {
-          if(day.appointments.includes(appointment.id) && appointment.interview === null){
-            return accumulator + 1
-          } else {
-            return accumulator
-          }
-        }, 0)
+      {props.days.map((day) => {
+        const spots = Object.values(props.appointments).reduce(
+          (accumulator, appointment) => {
+            if (
+              day.appointments.includes(appointment.id) &&
+              appointment.interview === null
+            ) {
+              return accumulator + 1;
+            } else {
+              return accumulator;
+            }
+          },
+          0
+        );
         return (
           <>
             <DayListItem
               name={day.name}
               spots={spots}
               selected={day.name === props.day}
-              setDay={props.setDay} />
-          </>)
+              setDay={props.setDay}
+            />
+          </>
+        );
       })}
     </ul>
   );
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,27 +2,26 @@ import React from "react";
 
 import "components/DayListItem.scss";
 
-let classNames = require('classnames');
+let classNames = require("classnames");
 
 export default function DayListItem(props) {
-  const selectClass = classNames("day-list__item",
-  {"day-list__item--selected":props.selected},
-  {"day-list__item--full": props.spots === 0}
-)
-  
-    let number;
-    if(props.spots > 1){
-    number = props.spots + ' spots remaining';
-    } else if( props.spots === 0){
-    number = 'no spots remaining';
-    } else if(props.spots === 1) {
-    number = '1 spot remaining';
-    }
-    
+  const selectClass = classNames(
+    "day-list__item",
+    { "day-list__item--selected": props.selected },
+    { "day-list__item--full": props.spots === 0 }
+  );
+
+  let number;
+  if (props.spots > 1) {
+    number = props.spots + " spots remaining";
+  } else if (props.spots === 0) {
+    number = "no spots remaining";
+  } else if (props.spots === 1) {
+    number = "1 spot remaining";
+  }
+
   return (
-    <li 
-    className= {selectClass} 
-    onClick={() => props.setDay(props.name)}>
+    <li className={selectClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{number}</h3>
     </li>
