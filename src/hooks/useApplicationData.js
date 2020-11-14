0b3af35b1a90930d8061316f9bfb60f3f886a0b2
Nicholas Kotsos <nicholaskotsos@nicholass-air.lan>
2020-11-14 14:37:31 -0800

    removed logic that was moved to useapplication data

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,73 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 
 
 export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    spots: 5,
+    appointments: [],
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const bookInterview = (id, interview) => {
+
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview: interview })
+      .then((res, err) => {
+        console.log(res, "axios promise")
+        const appointment = {
+          ...state.appointments[id],
+          interview: { ...interview }
+        }
+        const appointments = {
+          ...state.appointments,
+          [id]: appointment
+        };
+        setState({
+          ...state,
+          appointments
+        });
+
+      })
+      .catch((err, res) => {
+
+        console.log(err, "sorry charlie")
+
+      })
+
+
+  };
+
+  const cancelInterview = (id) => {
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`,)
+      .then((res, err) => {
+
+        const appointment = {
+          ...state.appointments[id],
+          interview: { ...state.interview }
+        }
+        const appointments = {
+          ...state.appointments,
+          [id]: appointment
+        };
+        setState({
+          ...state,
+          appointments
+        });
+
+      })
+      .catch((err, res) => {
+
+        console.log(err, "sorry charlie")
+
+      })
+  }
+
   return {
     state: state,
     setDay: setDay,
@@ -11,68 +75,4 @@ export default function useApplicationData() {
     cancelInterview: cancelInterview
   }
 }
-
-const [state, setState] = useState({
-  day: "Monday",
-  days: [],
-  spots: 5,
-  appointments: [],
-  interviewers: {}
-});
-
-const setDay = day => setState({ ...state, day });
-
-const bookInterview = (id, interview) => {
-
-  return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview: interview })
-    .then((res, err) => {
-      console.log(res, "axios promise")
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...interview }
-      }
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      setState({
-        ...state,
-        appointments
-      });
-
-    })
-    .catch((err, res) => {
-
-      console.log(err, "sorry charlie")
-
-    })
-
-
-};
-
-const cancelInterview = (id) => {
-  return axios.delete(`http://localhost:8001/api/appointments/${id}`,)
-    .then((res, err) => {
-
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...state.interview }
-      }
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      setState({
-        ...state,
-        appointments
-      });
-
-    })
-    .catch((err, res) => {
-
-      console.log(err, "sorry charlie")
-
-    })
-}
-
 
